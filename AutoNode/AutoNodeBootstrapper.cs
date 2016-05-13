7fb5388a2a92290713f93b2fd625a03cab60f2ef
Sotiris Filippidis <sotirisf@dot-see.com>
2016-05-13 12:04:58 +0300

    Event handler and update readme.md

---
+++ AutoNode/AutoNodeBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace DotSee
+{
+    public class AutoNodeBootstrapper : ApplicationEventHandler
+    {
+
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            base.ApplicationStarted(umbracoApplication, applicationContext);
+
+            AutoNode au = AutoNode.Instance;
+
+            ContentService.Saved += ContentServiceSaved;
+
+        }
+
+        private void ContentServiceSaved(IContentService sender, SaveEventArgs<IContent> args)
+        {
+
+            foreach (IContent node in args.SavedEntities)
+            {
+                AutoNode.Instance.Run(node);
+            }
+        }
+
+
+    }
+}
